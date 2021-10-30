e40de18b520d17356bae5b6921356121ac641c61
Krititya Onbun-ua <krittiya.on@mail.kmutt.ac.th>
2021-10-30 15:05:12 +0700

    [FIX] conflict

--- lib/service/linerReg.dart
+++ lib/service/linerReg.dart
@@ -5,32 +5,30 @@ import 'dart:io';
 /// A simple usage example using synthetic data. To see more complex examples,
 /// please, visit other directories in this folder
 
-
 //class LinearReg{
 
-  // var dataFrame;
-  // LinearReg({this.dataFrame});
+// var dataFrame;
+// LinearReg({this.dataFrame});
 
-  Future m
-ain() async {
-    // Let's create a dataframe with fitting data, let's assume, that the target
-    // column is the fifth column (column with index 4)
-    print('Enter to continue..');
-    String? dataIp = stdin.readLineSync();
-    final dataFrame = DataFrame(<Iterable<num>>[
-      [2, 3, 4, 5, 4.3],
-      [12, 32, 1, 3, 3.5],
-      [27, 3, 0, 59, 2.1],
-    ], headerExists: false);
+Future main() async {
+  // Let's create a dataframe with fitting data, let's assume, that the target
+  // column is the fifth column (column with index 4)
+  print('Enter to continue..');
+  String? dataIp = stdin.readLineSync();
+  final dataFrame = DataFrame(<Iterable<num>>[
+    [2, 3, 4, 5, 4.3],
+    [12, 32, 1, 3, 3.5],
+    [27, 3, 0, 59, 2.1],
+  ], headerExists: false);
 
-    // Let's create a regressor itself and train it
-    final regressor = LinearRegressor(dataFrame, 'col_4',
-        iterationsLimit: 100,
-        initialLearningRate: 0.0005,
-        learningRateType: LearningRateType.constant);
+  // Let's create a regressor itself and train it
+  final regressor = LinearRegressor(dataFrame, 'col_4',
+      iterationsLimit: 100,
+      initialLearningRate: 0.0005,
+      learningRateType: LearningRateType.constant);
 
-    // Let's see adjusted coefficients
-    print('Regression coefficients: ${regressor.coefficients}');
-  }
+  // Let's see adjusted coefficients
+  print('Regression coefficients: ${regressor.coefficients}');
+}
 //}
 
