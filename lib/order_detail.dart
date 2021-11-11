31b9759134688b54482a6e7db73f32053367c7ab
Chantisa <chanti.bodyslam@gmail.com>
2021-11-11 17:14:56 +0700

    [WIP] order detail

---
+++ lib/order_detail.dart
@@ -0,0 +1,21 @@
+import 'package:csc344_project/add_product.dart';
+import 'package:csc344_project/notifier/product_notifier.dart';
+import 'package:csc344_project/service/database.dart';
+import 'package:flutter/material.dart';
+import 'package:csc344_project/style/color.dart';
+import 'package:csc344_project/style/font_style.dart';
+import 'package:csc344_project/widgets/appbar.dart';
+import 'package:provider/provider.dart';
+class orderDetail extends StatefulWidget {
+  const orderDetail({Key? key}) : super(key: key);
+
+  @override
+  _orderDetailState createState() => _orderDetailState();
+}
+
+class _orderDetailState extends State<orderDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
